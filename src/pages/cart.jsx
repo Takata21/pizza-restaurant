b59f343e7105bb70082cdd70fbe16fde12cef212
Manuel Takata <takata211011@gmail.com>
2023-02-13 22:43:40 -0500

    add data from api

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,6 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  usePayPalScriptReducer,
+} from '@paypal/react-paypal-js'
+
 function Cart() {
+  const [open, setOpen] = useState(false)
+  // This values are the props in the UI
+  const amount = '2'
+  const currency = 'USD'
+  const style = { layout: 'vertical' }
+  const dispatch = useDispatch()
+  const cart = useSelector((state) => state.cart)
+
+  // Custom component to wrap the PayPalButtons and handle currency changes
+  const ButtonWrapper = ({ currency, showSpinner }) => {
+    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
+    // This is the main reason to wrap the PayPalButtons in a new component
+    const [{ options, isPending }, dispatch] = usePayPalScriptReducer()
+
+    useEffect(() => {
+      dispatch({
+        type: 'resetOptions',
+        value: {
+          ...options,
+          currency: currency,
+        },
+      })
+    }, [currency, showSpinner])
+
+    return (
+      <>
+        {showSpinner && isPending && <div className="spinner" />}
+        <PayPalButtons
+          style={style}
+          disabled={false}
+          forceReRender={[amount, currency, style]}
+          fundingSource={undefined}
+          createOrder={(data, actions) => {
+            return actions.order
+              .create({
+                purchase_units: [
+                  {
+                    amount: {
+                      currency_code: currency,
+                      value: amount,
+                    },
+                  },
+                ],
+              })
+              .then((orderId) => {
+                // Your code here after create the order
+                return orderId
+              })
+          }}
+          onApprove={function (data, actions) {
+            return actions.order.capture().then(function () {
+              // Your code here after capture the order
+            })
+          }}
+        />
+      </>
+    )
+  }
   return (
     <div className="flex p-12 -container sm:flex-col sm:p-5">
       <div className="-left flex-[2]">
@@ -16,80 +82,50 @@ function Cart() {
             </tr>
           </thead>
           <tbody>
-            <tr className="-tr pt-5 sm:flex sm:flex-col sm:text-center sm:mb-5">
-              <td className="pb-5 pt-5">
-                <div className="relative w-24 h-24 -imgContainer sm:w-[35vw] sm:h-[35vw] sm:m-auto">
-                  <Image
-                    src="/img/pizza.png"
-                    fill
-                    alt=""
-                    style={{ objectFit: 'cover' }}
-                  />
-                </div>
-              </td>
-              <td>
-                <span className="-name sm:text-base font-medium text-lg text-[#d1411e]">
-                  CORALZO
-                </span>
-              </td>
-              <td>
-                <span className="-extras sm:text-2xl">
-                  Double ingredient, spicy sauce
-                </span>
-              </td>
-              <td>
-                <span className="-price sm:before:content-['Price:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
-                  $19.90
-                </span>
-              </td>
-              <td>
-                <span className="-quantity sm:before:content-['Quantity:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
-                  2
-                </span>
-              </td>
-              <td>
-                <span className="text-lg font-medium -total sm:before:content-['Total:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
-                  $39.80
-                </span>
-              </td>
-            </tr>
-            <tr className="-tr pt-5 sm:flex sm:flex-col sm:text-center sm:mb-5">
-              <td className="pb-5">
-                <div className="relative w-24 h-24 -imgContainer sm:w-[35vw] sm:h-[35vw] sm:m-auto">
-                  <Image
-                    src="/img/pizza.png"
-                    fill
-                    alt=""
-                    style={{ objectFit: 'cover' }}
-                  />
-                </div>
-              </td>
-              <td>
-                <span className="-name sm:text-base font-medium text-lg text-[#d1411e]">
-                  CORALZO
-                </span>
-              </td>
-              <td>
-                <span className="-extras sm:text-2xl">
-                  Double ingredient, spicy sauce
-                </span>
-              </td>
-              <td>
-                <span className="-price sm:before:content-['Price:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
-                  $19.90
-                </span>
-              </td>
-              <td>
-                <span className="-quantity sm:before:content-['Quantity:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
-                  2
-                </span>
-              </td>
-              <td>
-                <span className="text-lg font-medium -total sm:before:content-['Total:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
-                  $39.80
-                </span>
-              </td>
-            </tr>
+            {cart.products.map((product) => (
+              <tr
+                className="-tr pt-5 sm:flex sm:flex-col sm:text-center sm:mb-5"
+                key={product._id}
+              >
+                <td className="pb-5">
+                  <div className="relative w-24 h-24 -imgContainer sm:w-[35vw] sm:h-[35vw] sm:m-auto">
+                    <Image
+                      src={product.img}
+                      fill
+                      alt=""
+                      style={{ objectFit: 'cover' }}
+                    />
+                  </div>
+                </td>
+                <td>
+                  <span className="-name sm:text-base font-medium text-lg text-[#d1411e]">
+                    {product.title}
+                  </span>
+                </td>
+                <td>
+                  <span className="-extras sm:text-2xl">
+                    {product.extras.map((extra) => (
+                      <span key={extra._id}>{extra.text} </span>
+                    ))}
+                  </span>
+                </td>
+                <td>
+                  <span className="-price sm:before:content-['Price:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
+                    {product.price}
+                  </span>
+                </td>
+                <td>
+                  <span className="-quantity sm:before:content-['Quantity:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
+                    {product.quantity}
+                  </span>
+                </td>
+                <td>
+                  <span className="text-lg font-medium -total sm:before:content-['Total:'] sm:before:font-medium sm:before:mr-1 sm:text-2xl">
+                    ${product.price * product.quantity}
+                  </span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -97,17 +133,39 @@ function Cart() {
         <div className="-wrapper w-[90%] max-h-[300px] bg-[#333] p-12 pt-3 flex flex-col justify-between text-white sm:m-auto">
           <h2 className="-title">CART TOTAL</h2>
           <div className="-totalText">
-            <b className="-totalTextTitle ml-3">Subtotal:</b>$79.60
+            <b className="-totalTextTitle ml-3">Subtotal:</b>${cart.total}
           </div>
           <div className="-totalText">
             <b className="-totalTextTitle ml-3">Discount:</b>$0.00
           </div>
           <div className="-totalText">
-            <b className="-totalTextTitle ml-3">Total:</b>$79.60
+            <b className="-totalTextTitle ml-3">Total:</b>${cart.total}
           </div>
-          <button className="-button h-[30px] font-bold text-[#d1411e] bg-white mt-5">
-            CHECKOUT NOW!
-          </button>
+          {open ? (
+            <div className="flex mt-3 flex-col">
+              <button className="p-2 mb-1 text-teal-500 bg-white font-bold">
+                CASH ON DELIVERY
+              </button>
+              <PayPalScriptProvider
+                options={{
+                  'client-id':
+                    'AcaiogoJbvgJT3IXtmyW0TIao4ovVU4M-nwZONsfnzO1T83filZkdiJX2Oaw5d2kn3WluaefZPO_m9VR',
+                  components: 'buttons',
+                  currency: 'USD',
+                  'disable-funding': 'credit,card,p24',
+                }}
+              >
+                <ButtonWrapper currency={currency} showSpinner={false} />
+              </PayPalScriptProvider>
+            </div>
+          ) : (
+            <button
+              className="-button h-[30px] font-bold text-[#d1411e] bg-white mt-5"
+              onClick={() => setOpen(true)}
+            >
+              CHECKOUT NOW!
+            </button>
+          )}
         </div>
       </div>
     </div>
--- src/pages/product/[id].jsx
+++ src/pages/product/[id].jsx
@@ -1,14 +1,35 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
-
-function Product() {
+import { useDispatch } from 'react-redux'
+import { addProduct } from '../../redux/cartSlice'
+function Product({ pizza }) {
   const [size, setSize] = useState(0)
-  const pizza = {
-    id: 1,
-    img: '/img/pizza.png',
-    name: 'CAMPAGNOLA',
-    price: [19.9, 23.9, 27.9],
-    dec: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt recusandae tenetur dolorum, ex ipsum dolorem! Eos eligendi quis impedit vel.',
+  const [price, setPrice] = useState(pizza.prices[0])
+  const [extras, setExtras] = useState([])
+  const [quantity, setQuantity] = useState(1)
+  const dispatch = useDispatch()
+  const changePrice = (number) => {
+    setPrice(price + number)
+  }
+  const handleChange = (e, option) => {
+    const checked = e.target.checked
+
+    if (checked) {
+      changePrice(option.price)
+      setExtras((prev) => [...prev, option])
+    } else {
+      setExtras(extras.filter((extra) => extra._id !== option._id))
+      changePrice(-option.price)
+    }
+  }
+
+  const handleSizes = (sizeIndex) => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size]
+    setSize(sizeIndex)
+    changePrice(difference)
+  }
+  const handleClick = () => {
+    dispatch(addProduct({ ...pizza, extras, price, quantity }))
   }
   return (
     <section className="h-[calc(100vh_-_100px)] flex sm:h-auto sm:text-center sm:flex-col sm:mt-5">
@@ -18,14 +39,17 @@ function Product() {
         </div>
       </section>
       <section className="flex-1 p-5 -right">
-        <h1 className="my-4 text-3xl font-bold -title sm:m-1">{pizza.name}</h1>
+        <h1 className="my-4 text-3xl font-bold -title sm:m-1">{pizza.title}</h1>
         <span className="-price text-[#d1411e] text-2xl font-normal border-b border-[#d1411e] my-3 inline-block">
-          ${pizza.price[size]}
+          ${price}
         </span>
-        <p className="-desc">{pizza.dec}</p>
+        <p className="-desc">{pizza.desc}</p>
         <h3 className="my-4 text-2xl font-bold -choose">Choose the size</h3>
         <div className="flex justify-between w-2/5 -sizes sm:w-full sm:px-5">
-          <button className="relative w-8 h-8 -size" onClick={() => setSize(0)}>
+          <button
+            className="relative w-8 h-8 -size"
+            onClick={() => handleSizes(0)}
+          >
             <Image src="/img/size.png" fill alt="" />
             <span className="-number flex justify-center items-center absolute top-[-5px] text-xs right-[-20px] text-white px-1 rounded-xl bg-teal-500">
               Small
@@ -33,7 +57,7 @@ function Product() {
           </button>
           <button
             className="-size w-[40px] h-[40px] relative"
-            onClick={() => setSize(1)}
+            onClick={() => handleSizes(1)}
           >
             <Image src="/img/size.png" fill alt="" />
             <span className="-number flex justify-center items-center absolute top-[-5px] text-xs right-[-20px] text-white px-1 rounded-xl bg-teal-500">
@@ -42,7 +66,7 @@ function Product() {
           </button>
           <button
             className="-size w-[50px] h-[50px] relative"
-            onClick={() => setSize(2)}
+            onClick={() => handleSizes(2)}
           >
             <Image src="/img/size.png" fill alt="" />
             <span className="-number flex justify-center items-center absolute top-[-5px] text-xs right-[-20px] text-white px-1 rounded-xl bg-teal-500">
@@ -54,58 +78,35 @@ function Product() {
           Choose additional ingredients
         </h3>
         <div className="flex mb-8 -ingredients sm:flex-col">
-          <div className="flex items-center gap-2 mr-3 text-sm font-medium -option sm:m-3 sm:text-lg">
-            <input
-              type="checkbox"
-              id="double"
-              name="double"
-              className="w-5 h-5 -checkbox sm:w-6 sm:h-6"
-            />
-            <label className="select-none" htmlFor="double">
-              Double Ingredients
-            </label>
-          </div>
-          <div className="flex items-center gap-2 mr-3 text-sm font-medium -option sm:m-3 sm:text-lg">
-            <input
-              className="w-5 h-5 -checkbox sm:w-6 sm:h-6"
-              type="checkbox"
-              id="cheese"
-              name="cheese"
-            />
-            <label className="select-none" htmlFor="cheese">
-              Extra Cheese
-            </label>
-          </div>
-          <div className="flex items-center gap-2 mr-3 text-sm font-medium -option sm:m-3 sm:text-lg">
-            <input
-              className="w-5 h-5 -checkbox sm:w-6 sm:h-6"
-              type="checkbox"
-              id="spicy"
-              name="spicy"
-            />
-            <label className="select-none" htmlFor="spicy">
-              Spicy Sauce
-            </label>
-          </div>
-          <div className="flex items-center gap-2 mr-3 text-sm font-medium -option sm:m-3 sm:text-lg">
-            <input
-              className="w-5 h-5 -checkbox sm:w-6 sm:h-6"
-              type="checkbox"
-              id="garlic"
-              name="garlic"
-            />
-            <label className="select-none" htmlFor="garlic">
-              Garlic Sauce
-            </label>
-          </div>
+          {pizza.extraOptions.map((option) => (
+            <div
+              className="flex items-center gap-2 mr-3 text-sm font-medium -option sm:m-3 sm:text-lg"
+              key={pizza._id}
+            >
+              <input
+                type="checkbox"
+                id={option.text}
+                name={option.text}
+                className="w-5 h-5 -checkbox sm:w-6 sm:h-6"
+                onChange={(e) => handleChange(e, option)}
+              />
+              <label className="select-none" htmlFor="double">
+                Double Ingredients
+              </label>
+            </div>
+          ))}
         </div>
-        <div className="flex items-center -add sm:justify-center">
+        <div className="flex items-center  -add sm:justify-center">
           <input
             type="number"
             defaultValue={1}
             className="w-12 -quantity h-7 sm:h-12 sm:px-5 sm:py-3"
+            onChange={() => setQuantity(e.target.value)}
           />
-          <button className="-button p-2 flex items-center justify-center rounded-sm h-7 ml-3 bg-[#d1411e] text-white border-none font-medium sm:h-12 sm:px-5 sm:py-3">
+          <button
+            className="-button p-2 flex items-center justify-center rounded-sm h-7 ml-3 bg-[#d1411e] text-white border-none font-medium sm:h-12 sm:px-5 sm:py-3"
+            onClick={handleClick}
+          >
             Add to Cart
           </button>
         </div>
@@ -114,4 +115,14 @@ function Product() {
   )
 }
 
+export const getServerSideProps = async ({ params }) => {
+  const res = await fetch(`http://localhost:3000/api/products/${params.id}`)
+  const data = await res.json()
+  console.log(data)
+  return {
+    props: {
+      pizza: data,
+    },
+  }
+}
 export default Product
