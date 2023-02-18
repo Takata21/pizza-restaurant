04064205291271efd0e666447febc245380b182b
Manuel Takata <takata211011@gmail.com>
2023-02-18 14:34:54 -0500

    add orders  updates

--- src/pages/api/orders/[id].js
+++ src/pages/api/orders/[id].js
@@ -16,18 +16,25 @@ const handler = async (req, res) => {
     }
   }
   if (method === 'PUT') {
+    console.log(req.body)
     try {
       const order = await Order.findByIdAndUpdate(id, req.body, {
         new: true,
       })
+      console.log(order)
       res.status(200).json(order)
     } catch (err) {
       res.status(500).json(err)
     }
   }
   if (method === 'DELETE') {
-  }
-  if (method === 'DELETE') {
+    try {
+      await Order.findByIdAndDelete(id)
+      res.status(200).json('The order has been delete')
+    } catch (err) {
+      console.log(err.message)
+      res.status(500)
+    }
   }
 }
 export default handler
