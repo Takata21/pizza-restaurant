8d68e61533a724be6d46ec0b5b691a6bf4a18c6f
Manuel Takata <takata211011@gmail.com>
2023-02-23 21:32:45 -0500

    add cookie

--- src/pages/api/products/index.js
+++ src/pages/api/products/index.js
@@ -2,7 +2,8 @@
 import dbConnect from '../../../utils/mongo'
 import Products from '../../../models/Product'
 export default async function handler(req, res) {
-  const { method } = req
+  const { method, cookies } = req
+  const token = cookies.token
   dbConnect()
   if (method === 'GET') {
     try {
@@ -14,6 +15,9 @@ export default async function handler(req, res) {
     }
   }
   if (method === 'POST') {
+    if (!token || token !== process.env.TOKEN) {
+      return res.status(401).json('Not authenticated!')
+    }
     try {
       const product = await Products.create(req.body)
       res.status(201).json(product)
